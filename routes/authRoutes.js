30953fb0e06255f5f571868759bfc5326c68b82a
Zahooruddin-dev <mzkhan886@gmail.com>
2026-01-15 20:22:11 +0500

    <S>

---
+++ routes/authRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const passport = require('passport');
+const {
+	registerControl,
+} = require('../controller/authControls');
+
+const router = express.Router();
+
+router.get('/register', checkNotAuthenticated, (req, res) => {
+	res.render('register');
+});
+
+router.get('/login', checkNotAuthenticated, (req, res) => {
+	res.render('login');
+});
+
+router.post('/register', checkNotAuthenticated, registerControl);
+
+router.post(
+	'/login',
+	checkNotAuthenticated,
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/api/auth/login',
+		failureFlash: true,
+	})
+);
+
+function checkNotAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return res.redirect('/');
+	}
+	next();
+}
+
+module.exports = router;
