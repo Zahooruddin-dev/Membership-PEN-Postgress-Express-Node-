a1db1b70a17d5a5b17b56413aa19a8f7d98c0cd6
Zahooruddin-dev <mzkhan886@gmail.com>
2026-01-15 20:21:45 +0500

    <S>

--- server.js
+++ server.js
@@ -1,93 +1,58 @@
 const path = require('node:path');
 const express = require('express');
-const bcrypt = require('bcrypt');
 const session = require('express-session');
 const passport = require('passport');
 const flash = require('express-flash');
-const {
-	registerControl,
-	loginControl,
-	createMessage,
-	showAllMessages,
-} = require('./controller/authControls');
-const db = require('./db/quries');
+
 const initializePassport = require('./passport-config');
+const db = require('./db/quries');
+
+const authRoutes = require('./routes/authRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+
+const app = express();
+
 initializePassport(
 	passport,
-	async (email) => {
-		const user = await db.findUserByEmail(email);
-		return user;
-	},
-	async (id) => {
-		const user = await db.findUserById(id);
-		return user;
-	}
+	async (email) => await db.findUserByEmail(email),
+	async (id) => await db.findUserById(id)
 );
-const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+
+app.use(
+	session({
+		secret: 'secret',
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/', checkAuthenticated, (req, res) => {
-	res.render('index', { name: req.user?.name });
+	res.render('index', { user: req.user });
 });
 
-app.get('/users', (req, res) => {
-	res.json(users);
-});
-
-app.get('/api/auth/register', checkNotAuthenticated, async (req, res) => {
-	res.render('register');
-});
-app.get('/api/auth/login', checkNotAuthenticated, async (req, res) => {
-	res.render('login');
-});
-
-app.post('/api/auth/register', checkNotAuthenticated, registerControl);
-app.post(
-	'/api/auth/login',
-	checkNotAuthenticated,
-	passport.authenticate('local', {
-		successRedirect: '/',
-		failureRedirect: '/api/auth/login',
-		failureFlash: true,
-	})
-);
-
-app.get('/api/create/message', async (req, res) => {
-	res.render('message');
-});
-app.post('/api/create/message', createMessage);
-
-app.get('/messages', showAllMessages);
+app.use('/api/auth', authRoutes);
+app.use('/api', messageRoutes);
 
 app.delete('/logout', (req, res, next) => {
 	req.logout((err) => {
-		if (err) {
-			return next(err);
-		}
-		res.redirect('/login');
+		if (err) return next(err);
+		res.redirect('/api/auth/login');
 	});
 });
 
 function checkAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
+	if (req.isAuthenticated()) return next();
 	res.redirect('/api/auth/login');
-}
-
-function checkNotAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) {
-		return res.redirect('/');
-	}
-	next();
 }
 
 app.listen(3000, () => {
