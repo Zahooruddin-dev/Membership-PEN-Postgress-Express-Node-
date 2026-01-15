e5850edfb8e604ede865fbe981d00398d83c1fdc
Zahooruddin-dev <mzkhan886@gmail.com>
2026-01-16 00:48:56 +0500

    <S>

--- db/pools.js
+++ db/pools.js
@@ -3,18 +3,18 @@ const { Pool } = require('pg');
 let pool;
 
 if (process.env.DATABASE_URL) {
-  // Production (Render or any cloud Postgres) - enable SSL
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+	// Production (Render or any cloud Postgres) - enable SSL
+	pool = new Pool({
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
 } else {
-  // Local development
-  pool = new Pool({
-    connectionString: 'postgresql://postgres:root@localhost:5432/membership',
-  });
+	// Local development
+	pool = new Pool({
+		connectionString: 'postgresql://postgres:oFpEZxOfEOofClVrFvOmZAyUHfbUyLDz@shuttle.proxy.rlwy.net:11624/railway',
+	});
 }
 
 module.exports = pool;
