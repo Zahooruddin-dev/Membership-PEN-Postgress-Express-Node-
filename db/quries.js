1b28f322bb498491bf53ff77693b49455a2a7cf7
Zahooruddin-dev <mzkhan886@gmail.com>
2026-01-15 20:15:25 +0500

    <S>

--- db/quries.js
+++ db/quries.js
@@ -1,38 +1,44 @@
 const pool = require('./pools');
 
-async function createUser(username, email, hashedPassword, membership_status= 'Premium') {
-  const result = await pool.query(
-    'INSERT INTO users (username, email, password, membership_status) VALUES ($1, $2, $3, $4) RETURNING *',
-    [username, email, hashedPassword, membership_status]
-  );
-  return result.rows[0];
+async function createUser(
+	username,
+	email,
+	hashedPassword,
+	membership_status = 'Premium'
+) {
+	const result = await pool.query(
+		'INSERT INTO users (username, email, password, membership_status) VALUES ($1, $2, $3, $4) RETURNING *',
+		[username, email, hashedPassword, membership_status]
+	);
+	return result.rows[0];
 }
 
 async function findUserByEmail(email) {
-  const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-  return result.rows[0];
+	const result = await pool.query('SELECT * FROM users WHERE email = $1', [
+		email,
+	]);
+	return result.rows[0];
 }
 async function createMessage(title, message) {
-  const result = await pool.query(
-    'INSERT INTO message (title, message) VALUES ($1, $2) RETURNING *',
-    [title, message]
-  );
-  return result.rows[0];
+	const result = await pool.query(
+		'INSERT INTO message (title, message) VALUES ($1, $2) RETURNING *',
+		[title, message]
+	);
+	return result.rows[0];
 }
 async function findUserById(id) {
-  const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-  return result.rows[0];
-}   
+	const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+	return result.rows[0];
+}
 async function allMessages() {
-  const result = await pool.query('SELECT * FROM message');
-  return result.rows;
+	const result = await pool.query('SELECT * FROM message ORDER BY id DESC');
+	return result.rows;
 }
 
-
 module.exports = {
-  createUser,
-  findUserByEmail,
-  createMessage,
-  findUserById,
-  allMessages,
+	createUser,
+	findUserByEmail,
+	createMessage,
+	findUserById,
+	allMessages,
 };
